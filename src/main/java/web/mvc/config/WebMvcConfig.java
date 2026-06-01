458108cb8e7327fa44e4ee035e218386d10c5aa6
hyuk-dev <hyuk.development@gmail.com>
2026-06-01 20:09:16 +0900

    fix: CORS

--- src/main/java/web/mvc/config/WebMvcConfig.java
+++ src/main/java/web/mvc/config/WebMvcConfig.java
@@ -19,8 +19,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
      registry.addMapping("/**")
     // .allowedOrigins("http://localhost:5173","http://localhost:4173")
      //.allowedOrigins("http://43.200.184.113:5173", "http://43.200.184.113:4173")
-      .allowedOrigins("http://3.35.209.95", "http://3.35.209.95:80")
-      .allowedOrigins("http://heejung.n-e.kr", "https://heejung.n-e.kr")
+      .allowedOrigins("http://3.35.209.95")
+//      .allowedOrigins("http://heejung.n-e.kr", "https://heejung.n-e.kr")
      .allowedMethods("OPTIONS","GET","POST","PUT","DELETE");
   }
 }
