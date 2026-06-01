74b1ecdc0b5fa9a4a10f940c458bc449613a7496
hyuk-dev <hyuk.development@gmail.com>
2026-06-01 16:09:51 +0900

    feat: CORS add EC2 Host

--- src/main/java/web/mvc/config/SecurityConfig.java
+++ src/main/java/web/mvc/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
                         CorsConfiguration configuration = new CorsConfiguration();
                         //configuration.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
                         //configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:4173"));
-                        configuration.setAllowedOrigins(Arrays.asList("http://43.203.170.229", "http://43.203.170.229:80"));
+                        configuration.setAllowedOrigins(Arrays.asList("http://13.124.233.98", "http://13.124.233.98:8080"));
                         configuration.setAllowedOrigins(Arrays.asList("http://heejung.n-e.kr", "https://heejung.n-e.kr"));
                         configuration.setAllowedMethods(Collections.singletonList("*"));
                         configuration.setAllowCredentials(true);
